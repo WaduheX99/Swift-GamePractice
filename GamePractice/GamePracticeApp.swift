66327a684101900e975ae7007441d3e74f926022
WaduheX99 <affandifaza@gmail.com>
2024-05-14 19:39:20 +0700

    Initial Commit

---
+++ GamePractice/GamePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePracticeApp.swift
+//  GamePractice
+//
+//  Created by Faza Faresha Affandi on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GamePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
